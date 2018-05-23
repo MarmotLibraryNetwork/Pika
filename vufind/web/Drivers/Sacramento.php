9d3705c3ccbaa9df617ee8f04a3e197108a4cbca
Pascal Brammeier <pascal@marmot.org>
2018-05-23 16:31:10 -0600

    Add Initial Sacramento Driver #D-2228
    https://marmot.myjetbrains.com/youtrack/issue/D-2228

---
+++ vufind/web/Drivers/Sacramento.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ *
+ * @category Pika
+ * @author: Pascal Brammeier
+ * Date: 5/23/2018
+ *
+ */
+
+require_once ROOT_DIR . '/Drivers/Sierra.php';
+
+class Sacramento extends Sierra
+{
+
+}
