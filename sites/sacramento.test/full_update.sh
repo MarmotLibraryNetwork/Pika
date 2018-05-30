f7f01f270f70d6237e9304c1a4004a730e80672e
Pascal Brammeier <pascal@marmot.org>
2018-05-30 09:25:56 -0600

    update to sacramento test fullupdate #D-2228
    https://marmot.myjetbrains.com/youtrack/issue/D-2228

--- sites/sacramento.test/full_update.sh
+++ sites/sacramento.test/full_update.sh
@@ -107,7 +107,10 @@ cd /usr/local/vufind-plus/vufind/record_grouping; java -server -XX:+UseG1GC -jar
 #MDN 4/12/2018 run everyday while we are changing settings
 #if [ "${DAYOFWEEK}" -eq 5 ];
 #then
+
 cd /usr/local/vufind-plus/vufind/reindexer; nice -n -3 java -server -XX:+UseG1GC -jar reindexer.jar ${PIKASERVER} fullReindex >> ${OUTPUT_FILE}
+cd /usr/local/vufind-plus/vufind/reindexer; java -server -XX:+UseG1GC -jar reindexer.jar ${PIKASERVER} fullReindex >> ${OUTPUT_FILE}
+
 #else
 #cd /usr/local/vufind-plus/vufind/reindexer; nice -n -3 java -server -XX:+UseG1GC -jar reindexer.jar ${PIKASERVER} >> ${OUTPUT_FILE}
 #fi
