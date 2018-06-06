9000763bd98d35f2daf898f0c97af07ee62a0fbf
Pascal Brammeier <pascal@marmot.org>
2018-06-06 15:24:30 -0600

    Sacramento Indexing #D-2233
    https://marmot.myjetbrains.com/youtrack/issue/D-2233

--- vufind/reindexer/src/org/vufind/SacramentoRecordProcess.java
+++ vufind/reindexer/src/org/vufind/SacramentoRecordProcess.java
@@ -3,7 +3,11 @@ package org.vufind;
 import org.apache.log4j.Logger;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
 
 /**
  * Custom Record Processing for Santa Fe
@@ -14,9 +18,32 @@ import java.sql.ResultSet;
  */
 
 class SacramentoRecordProcessor extends IIIRecordProcessor {
+    private HashSet<String> recordsWithVolumes = new HashSet<>();
 
     SacramentoRecordProcessor(GroupedWorkIndexer indexer, Connection vufindConn, ResultSet indexingProfileRS, Logger logger, boolean fullReindex) {
         super(indexer, vufindConn, indexingProfileRS, logger, fullReindex);
+
+        loadOrderInformationFromExport();
+
+        loadVolumesFromExport(vufindConn);
+
+        validCheckedOutStatusCodes.add("d");
+        validCheckedOutStatusCodes.add("o");
+
+    }
+
+    private void loadVolumesFromExport(Connection vufindConn){
+        try{
+            PreparedStatement loadVolumesStmt = vufindConn.prepareStatement("SELECT distinct(recordId) FROM ils_volume_info", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+            ResultSet volumeInfoRS = loadVolumesStmt.executeQuery();
+            while (volumeInfoRS.next()){
+                String recordId = volumeInfoRS.getString(1);
+                recordsWithVolumes.add(recordId);
+            }
+            volumeInfoRS.close();
+        }catch (SQLException e){
+            logger.error("Error loading volumes from the export", e);
+        }
     }
 
     @Override
@@ -29,7 +56,7 @@ class SacramentoRecordProcessor extends IIIRecordProcessor {
         boolean available = false;
         String status = itemInfo.getStatusCode();
         String dueDate = itemInfo.getDueDate() == null ? "" : itemInfo.getDueDate();
-        String availableStatus = "-o";
+        String availableStatus = "-ocd(j";
         if (status.length() > 0 && availableStatus.indexOf(status.charAt(0)) >= 0) {
             if (dueDate.length() == 0) {
                 available = true;
@@ -37,4 +64,9 @@ class SacramentoRecordProcessor extends IIIRecordProcessor {
         }
         return available;
     }
+
+    protected boolean determineLibraryUseOnly(ItemInfo itemInfo, Scope curScope) {
+        return itemInfo.getStatusCode().equals("o");
+    }
+
 }
