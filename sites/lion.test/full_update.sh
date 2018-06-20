2b70dbf9ea6dbb6066eea4e9cbbd49412b230100
Pascal Brammeier <pascal@marmot.org>
2018-06-20 11:20:50 -0600

    LION RBDigital for individual libraries #D-2332
    https://marmot.myjetbrains.com/youtrack/issue/D-2332

--- sites/lion.test/full_update.sh
+++ sites/lion.test/full_update.sh
@@ -87,6 +87,8 @@ cd /usr/local/vufind-plus/vufind/cron;./GetHooplaFromMarmot.sh >> ${OUTPUT_FILE}
 
 # RBDigital Marc Updates
 /usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} lion/rbdigital_audio rbdigital_audio/lion >> ${OUTPUT_FILE}
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} lion/rbdigital/russell rbdigital/russell >> ${OUTPUT_FILE}
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} lion/rbdigital/easthampton rbdigital/easthampton >> ${OUTPUT_FILE}
 /usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} lion/rbdigital_magazine rbdigital_magazine/lion >> ${OUTPUT_FILE}
 
 #Extract Lexile Data
