88de090094acd568651e9c305dbf43a941236846
NashvillePublicLibrary <james.staub@nashville.gov>
2018-08-16 12:29:07 -0500

    Adds BNJ to Nashville Pika email alerts
    https://trello.com/c/sT1Muil5

--- sites/nashville.production/full_update.sh
+++ sites/nashville.production/full_update.sh
@@ -29,7 +29,7 @@
 
 
 # this version emails script output as a round finishes
-EMAIL=james.staub@nashville.gov,Pascal.Brammeier@nashville.gov
+EMAIL=james.staub@nashville.gov,bryan.n.jones@nashville.gov,Pascal.Brammeier@nashville.gov
 PIKASERVER=nashville.production
 PIKADBNAME=vufind
 OUTPUT_FILE="/var/log/pika/${PIKASERVER}/full_update_output.log"
