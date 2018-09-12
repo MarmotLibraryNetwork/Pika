92b8bd6bebaa2f514c55e37474ccbb7240d1144d
Pascal Brammeier <pascal@marmot.org>
2018-09-12 10:18:28 -0600

    Mesa Mango Sideload #D-2593
    https://marmot.myjetbrains.com/youtrack/issue/D-2593

--- sites/marmot.test/full_update.sh
+++ sites/marmot.test/full_update.sh
@@ -158,6 +158,10 @@ cd /usr/local/vufind-plus/vufind/cron;./HOOPLA.sh ${PIKASERVER} >> ${OUTPUT_FILE
 /usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} fortlewis_sideload/naxos naxos/fortlewis >> ${OUTPUT_FILE}
 /usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} fortlewis_sideload/naxos_jazz naxos/jazz/fortlewis >> ${OUTPUT_FILE}
 
+#Mango
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} mesa/mango mango/mesa >> ${OUTPUT_FILE}
+
+
 #Extracts for sideloaded eContent; settings defined in config.pwd.ini [Sideload]
 cd /usr/local/vufind-plus/vufind/cron; ./sideload.sh ${PIKASERVER}
 
