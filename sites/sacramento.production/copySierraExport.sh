f256e63d1a1e59b6daabaead4fea4f7030581efd
Pascal Brammeier <pascal@marmot.org>
2018-09-24 12:07:39 -0600

    Set up Nightly Full export for Sacramento #D-2496
    https://marmot.myjetbrains.com/youtrack/issue/D-2496

--- sites/sacramento.production/copySierraExport.sh
+++ sites/sacramento.production/copySierraExport.sh
@@ -4,6 +4,7 @@ mount 10.1.2.7:/ftp/sacramento/sierra /mnt/ftp
 # sftp.marmot.org server
 
 #copy production extract
+FILE1=$(find /mnt/ftp/ -name *.mrc -mtime -1 | sort -n | tail -1)
 cp --preserve=timestamps --update /mnt/ftp/fullexport.mrc /data/vufind-plus/sacramento.production/marc/fullexport.mrc
 umount /mnt/ftp
 
