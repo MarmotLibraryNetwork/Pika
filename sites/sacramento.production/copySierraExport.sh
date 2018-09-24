2d1af2b4ea846a166e2aebaa0faef5329520a5db
Pascal Brammeier <pascal@marmot.org>
2018-09-24 13:34:12 -0600

    fix copy full export continued #D-2496

--- sites/sacramento.production/copySierraExport.sh
+++ sites/sacramento.production/copySierraExport.sh
@@ -5,6 +5,6 @@ mount 10.1.2.7:/ftp/sacramento/sierra /mnt/ftp
 
 #copy production extract
 FILE1=$(find /mnt/ftp/ -name *.mrc -mtime -1 | sort -n | tail -1)
-cp --preserve=timestamps --update /mnt/ftp/${FILE1} /data/vufind-plus/sacramento.production/marc/fullexport.mrc
+cp --preserve=timestamps --update /${FILE1} /data/vufind-plus/sacramento.production/marc/fullexport.mrc
 umount /mnt/ftp
 
