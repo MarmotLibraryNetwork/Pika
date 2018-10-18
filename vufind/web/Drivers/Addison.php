598c890e5d70b733113353517726589b97e281bb
Pascal Brammeier <pascal@marmot.org>
2018-10-18 15:31:27 -0600

    Addison Implementation #PRJ-49
    https://marmot.myjetbrains.com/youtrack/issue/PRJ-49

--- vufind/web/Drivers/Addison.php
+++ vufind/web/Drivers/Addison.php
@@ -12,4 +12,13 @@
 class Addison extends Sierra
 {
 
+	public function _getLoginFormValues($patron){
+		$loginData = array();
+		$loginData['code'] = $patron->cat_username;
+		$loginData['pin']  = $patron->cat_password;
+//		$loginData['pat_submit']  = 'xxx';
+
+		return $loginData;
+	}
+
 }
