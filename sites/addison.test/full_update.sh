6644003d9dc6696f60babf39330683f5bcb73ad6
Pascal Brammeier <pascal@marmot.org>
2018-10-19 13:44:16 -0600

    Fetch hoopla data for Addison #D-2683
    https://marmot.myjetbrains.com/youtrack/issue/D-2683

--- sites/addison.test/full_update.sh
+++ sites/addison.test/full_update.sh
@@ -123,7 +123,7 @@ cd /usr/local/vufind-plus/sites/${PIKASERVER}; ./${PIKASERVER}.sh restart
 #nice -n -10 java -jar cron.jar ${PIKASERVER} ExportSierraData >> ${OUTPUT_FILE}
 
 #Extract from Hoopla
-#cd /usr/local/vufind-plus/vufind/cron;./GetHooplaFromMarmot.sh >> ${OUTPUT_FILE}
+cd /usr/local/vufind-plus/vufind/cron;./GetHooplaFromMarmot.sh >> ${OUTPUT_FILE}
 
 #Extract Lexile Data
 cd /data/vufind-plus/; curl --remote-name --remote-time --silent --show-error --compressed --time-cond /data/vufind-plus/lexileTitles.txt https://cassini.marmot.org/lexileTitles.txt
