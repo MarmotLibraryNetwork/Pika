2e4ca199db114f167178009ccc51868fe848f59b
Pascal Brammeier <pascal@marmot.org>
2018-11-01 14:17:06 -0600

    minor typo and spacing

--- vufind/web/index.php
+++ vufind/web/index.php
@@ -803,13 +803,13 @@ function getGitBranch(){
 	global $interface;
 	global $configArray;
 
-	$gitName = $configArray['System']['gitVersionFile'];
+	$gitName    = $configArray['System']['gitVersionFile'];
 	$branchName = 'Unknown';
 	if ($gitName == 'HEAD'){
 		$stringFromFile = file('../../.git/HEAD', FILE_USE_INCLUDE_PATH);
 		$stringFromFile = $stringFromFile[0]; //get the string from the array
-		$explodedString = explode("/", $stringFromFile); //seperate out by the "/" in the string
-		$branchName = $explodedString[2]; //get the one that is always the branch name
+		$explodedString = explode("/", $stringFromFile); // separate out by the "/" in the string
+		$branchName     = $explodedString[2]; //get the one that is always the branch name
 	}else{
 		$stringFromFile = file('../../.git/FETCH_HEAD', FILE_USE_INCLUDE_PATH);
 		if (!empty($stringFromFile)) {
