2f6fe2a15def23baba84773b6a09e4603f5ccb7d
Pascal Brammeier <pascal@marmot.org>
2018-11-02 16:23:34 -0600

    RB Digtal sideloads #D-2666
    https://marmot.myjetbrains.com/youtrack/issue/D-2666

--- sites/wcpl.test/full_update.sh
+++ sites/wcpl.test/full_update.sh
@@ -123,6 +123,11 @@ cd /data/vufind-plus/; curl --remote-name --remote-time --silent --show-error --
 #Extract AR Data
 cd /data/vufind-plus/accelerated_reader; curl --remote-name --remote-time --silent --show-error --compressed --time-cond /data/vufind-plus/accelerated_reader/RLI-ARDataTAB.txt https://cassini.marmot.org/RLI-ARDataTAB.txt
 
+# Gale
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} wcpl/gale gale/wcpl >> ${OUTPUT_FILE}
+
+#RBDigital
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} wcpl/rbdigital rbdigital/wcpl >> ${OUTPUT_FILE}
 
 #Do a full extract from OverDrive just once a week to catch anything that doesn't
 #get caught in the regular extract
