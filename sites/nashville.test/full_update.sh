c370d12cf7cea67a5d728258a19e18cb7c1f9fbd
NashvillePublicLibrary <james.staub@nashville.gov>
2019-06-04 10:11:23 -0500

    Nashville minfilesize update
    https://marmot.myjetbrains.com/youtrack/issue/D-3079

--- sites/nashville.test/full_update.sh
+++ sites/nashville.test/full_update.sh
@@ -36,11 +36,7 @@ OUTPUT_FILE="/var/log/pika/${PIKASERVER}/full_update_output.log"
 DAYOFWEEK=$(date +"%u")
 
 # Actual CarlX extract size 2017 07 03 - 1021325895  - pascal
-MINFILE1SIZE=$((1070000000))
-# below values from millennium
-# JAMES set MIN 2016 11 03 actual extract size 825177201
-# JAMES set MIN 2017 01 31 actual extract size 823662098
-# JAMES set MIN 2017 02 01 actual extract size 817883489
+MINFILE1SIZE=$((1085000000))
 
 # determine whether this server is production or test
 CONFIG=/usr/local/VuFind-Plus/sites/${PIKASERVER}/conf/config.pwd.ini
