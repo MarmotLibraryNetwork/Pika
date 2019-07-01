5cece9de473793d92d2809242366e2a02257e2db
Chris <chris@marmot.org>
2019-07-01 11:17:59 -0600

    #D-3076 Gutenberg sideload

--- sites/aspencat.test/full_update.sh
+++ sites/aspencat.test/full_update.sh
@@ -89,6 +89,9 @@ cd /usr/local/vufind-plus/vufind/cron;./GetHooplaFromMarmot.sh >> ${OUTPUT_FILE}
 #Colorado State Government Documents Updates
 curl --remote-time --show-error --compressed -o /data/vufind-plus/colorado_gov_docs/marc/fullexport.mrc https://cassini.marmot.org/colorado_state_docs.mrc
 
+# Gutenberg
+/usr/local/vufind-plus/vufind/cron/fetch_sideload_data.sh ${PIKASERVER} aspencat/gutenberg gutenberg/aspencat >> ${OUTPUT_FILE}
+
 #Extract Lexile Data
 cd /data/vufind-plus/; curl --remote-name --remote-time --silent --show-error --compressed --time-cond /data/vufind-plus/lexileTitles.txt https://cassini.marmot.org/lexileTitles.txt
 
