f4e621c4400b17afddd02e4f03f9c22b51a9ca27
Pascal Brammeier <>
2019-07-30 14:35:56 -0600

    Overdrive Extractor needs an execution time limit when there are numerous changes so the continuous reindexing is happening often enough to keep ILS changes up to date. D-3171
    https://marmot.myjetbrains.com/youtrack/issue/D-3171

--- sites/marmot.test/continuous_partial_reindex.sh
+++ sites/marmot.test/continuous_partial_reindex.sh
@@ -14,29 +14,8 @@ USE_SIERRA_API_EXTRACT=1
 
 source "/usr/local/vufind-plus/vufind/bash/checkConflicts.sh"
 
-function sendEmail() {
-	# add any logic wanted for when to send the emails here. (eg errors only)
-	FILESIZE=$(stat -c%s ${OUTPUT_FILE})
-	if [[ ${FILESIZE} -gt 0 ]]
-	then
-			# send mail
-			mail -s "Continuous Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
-	fi
-}
-
-function checkForDBCrash() {
-# Pass this function the exit code ($?) of pika java programs.
-# If the exit code is zero that indicates that the pika database is down or unreachable,
-# so we will pause our operations here
-	EXITCODE=$1
-	if [ $EXITCODE -eq 2 ];then
-		sleep 180
-		echo "Received database connection lost error, paused for 180 seconds" >> ${OUTPUT_FILE}
-	fi
-}
-
 # Kick-off Sierra Extract/Reindex loop
-./sierra_continuous_reindex.sh &
+/usr/local/vufind-plus/sites/${PIKASERVER}/sierra_continuous_reindex.sh &
 
 # Kick-off Overdrive Extract/Reindex loop
-./overdrive_continuous_reindex.sh &
+/usr/local/vufind-plus/sites/${PIKASERVER}/overdrive_continuous_reindex.sh &
