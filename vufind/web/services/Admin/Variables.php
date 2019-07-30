37c366b429714e8cf9bc3769508be5e2fcc13d30
Pascal Brammeier <>
2019-07-30 09:31:11 -0600

    Add sset true/false buttons for system variables

--- vufind/web/services/Admin/Variables.php
+++ vufind/web/services/Admin/Variables.php
@@ -2,25 +2,27 @@
 /**
  * Display a list of internal variables that have been defined in the system.
  *
- * @category VuFind-Plus-2014
+ * @category Pika
  * @author Mark Noble <mark@marmot.org>
  * Date: 4/27/14
  * Time: 2:21 PM
  */
-require_once ROOT_DIR . '/Action.php';
 require_once ROOT_DIR . '/services/Admin/ObjectEditor.php';
 
-class Admin_Variables extends ObjectEditor{
+class Admin_Variables extends ObjectEditor {
 
 	function getObjectType(){
 		return 'Variable';
 	}
+
 	function getToolName(){
 		return 'Variables';
 	}
+
 	function getPageTitle(){
 		return 'System Variables';
 	}
+
 	function getAllObjects(){
 		$variableList = array();
 
@@ -32,33 +34,42 @@ class Admin_Variables extends ObjectEditor{
 		}
 		return $variableList;
 	}
+
 	function getObjectStructure(){
 		return Variable::getObjectStructure();
 	}
+
 	function getPrimaryKeyColumn(){
 		return 'name';
 	}
+
 	function getIdKeyColumn(){
 		return 'id';
 	}
+
 	function getAllowableRoles(){
 		return array('opacAdmin');
 	}
+
 	function canAddNew(){
 		return false;
 	}
+
 	function canDelete(){
 		$user = UserAccount::getLoggedInUser();
 		return UserAccount::userHasRole('opacAdmin');
 	}
 
-
+	/**
+	 * @param Variable $existingObject
+	 * @return array
+	 */
 	function getAdditionalObjectActions($existingObject){
 		$actions = array();
 		if ($existingObject && $existingObject->id != ''){
 			$actions[] = array(
 				'text' => '<span class="glyphicon glyphicon-time" aria-hidden="true"></span> Set to Current Timestamp (seconds)',
-				'url' => "/{$this->getModule()}/{$this->getToolName()}?objectAction=setToNow&amp;id=" . $existingObject->id,
+				'url'  => "/{$this->getModule()}/{$this->getToolName()}?objectAction=setToNow&amp;id=" . $existingObject->id,
 			);
 			$actions[] = array(
 				'text' => '<span class="glyphicon glyphicon-time" aria-hidden="true"></span> Set to Current Timestamp (milliseconds)',
@@ -72,17 +83,31 @@ class Admin_Variables extends ObjectEditor{
 				'text' => '<span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span> Decrease by 500',
 				'url'  => "/{$this->getModule()}/{$this->getToolName()}?objectAction=IncrementVariable&amp;direction=down&amp;id=" . $existingObject->id,
 			);
+			if ($existingObject->value == '1' || $existingObject->value == 'true'){
+				$actions[] = array(
+					'text' => '<span class="glyphicon glyphicon-minus-sign" aria-hidden="true"></span> Set to false',
+					'url'  => "/{$this->getModule()}/{$this->getToolName()}?objectAction=SwitchBooleanVariable&amp;id=" . $existingObject->id,
+				);
+			}elseif ( $existingObject->value == '0' || $existingObject->value = 'false'){
+				$actions[] = array(
+					'text' => '<span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Set to true',
+					'url'  => "/{$this->getModule()}/{$this->getToolName()}?objectAction=SwitchBooleanVariable&amp;id=" . $existingObject->id,
+				);
+			}
 		}
 		return $actions;
 	}
 
+	/**
+	 * Additional Object Action to set a variable to the current timestamp
+	 */
 	function setToNow(){
-		$id = $_REQUEST['id'];
+		$id              = $_REQUEST['id'];
 		$useMilliseconds = isset($_REQUEST['ms']) && ($_REQUEST['ms'] == 1 || $_REQUEST['ms'] == 'true');
-		if (!empty($id) && ctype_digit($id)) {
+		if (!empty($id) && ctype_digit($id)){
 			$variable = new Variable();
 			$variable->get($id);
-			if ($variable) {
+			if ($variable){
 				$variable->value = $useMilliseconds ? time() * 1000 : time();
 				$variable->update();
 			}
@@ -90,19 +115,22 @@ class Admin_Variables extends ObjectEditor{
 		}
 	}
 
+	/**
+	 * Additional Object Action to increase or decrease the value of a variable
+	 */
 	function IncrementVariable(){
 		$id = $_REQUEST['id'];
-		if (!empty($id) && ctype_digit($id)) {
+		if (!empty($id) && ctype_digit($id)){
 			$variable = new Variable();
 			$variable->get($id);
-			if ($variable) {
+			if ($variable){
 				$amount = 0;
-				if ($_REQUEST['direction'] == 'up') {
+				if ($_REQUEST['direction'] == 'up'){
 					$amount = 10000;
-				} elseif ($_REQUEST['direction'] == 'down') {
+				}elseif ($_REQUEST['direction'] == 'down'){
 					$amount = -500;
 				}
-				if ($amount) {
+				if ($amount){
 					$variable->value += $amount;
 					$variable->update();
 				}
@@ -111,13 +139,33 @@ class Admin_Variables extends ObjectEditor{
 		}
 	}
 
-	function editObject($objectAction, $structure)
-	{
-		if ($objectAction == 'save') {
-			if (!empty($_REQUEST['name']) && $_REQUEST['name'] == 'offline_mode_when_offline_login_allowed') {
-				if (!empty($_REQUEST['value']) && $_REQUEST['value'] == 'true' || $_REQUEST['value'] == 1) {
+	/**
+	 * An additional object action to switch a boolean valued variable
+	 */
+	function SwitchBooleanVariable(){
+		$id = $_REQUEST['id'];
+		if (!empty($id) && ctype_digit($id)){
+			$variable = new Variable();
+			$variable->get($id);
+			if ($variable){
+				if ($variable->value == '1' || $variable->value == 'true'){
+					$variable->value = '0';
+					$variable->update();
+				}elseif ($variable->value == '0' || $variable->value == 'false'){
+					$variable->value = '1';
+					$variable->update();
+				}
+			}
+			header("Location: /{$this->getModule()}/{$this->getToolName()}?objectAction=edit&id=" . $id);
+		}
+	}
+
+	function editObject($objectAction, $structure){
+		if ($objectAction == 'save'){
+			if (!empty($_REQUEST['name']) && $_REQUEST['name'] == 'offline_mode_when_offline_login_allowed'){
+				if (!empty($_REQUEST['value']) && $_REQUEST['value'] == 'true' || $_REQUEST['value'] == 1){
 					global $configArray;
-					if (isset($configArray['Catalog']['enableLoginWhileOffline']) && empty($configArray['Catalog']['enableLoginWhileOffline'])) {
+					if (isset($configArray['Catalog']['enableLoginWhileOffline']) && empty($configArray['Catalog']['enableLoginWhileOffline'])){
 						$_SESSION['lastError'] = "While offline logins are disabled offline mode can not be turned on with this variable.";
 						header("Location: {$configArray['Site']['path']}{$_SERVER['REQUEST_URI']}");
 						die();
