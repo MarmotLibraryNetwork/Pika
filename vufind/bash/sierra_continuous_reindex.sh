41087e298617e02300c4b91f1714176ef32d779a
Pascal Brammeier <>
2019-08-05 10:56:55 -0600

    refactor continous re-indexing bash scripts D-3171
    https://marmot.myjetbrains.com/youtrack/issue/D-3171

--- vufind/bash/sierra_continuous_reindex.sh
+++ vufind/bash/sierra_continuous_reindex.sh
@@ -1,32 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-EMAIL=root@titan
-PIKASERVER=marmot.test
-
-OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/overdrive_continuous_reindex_output.log"
+OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/sierra_continuous_reindex_output.log"
 
 source "/usr/local/vufind-plus/vufind/bash/checkConflicts.sh"
-
-function sendEmail() {
-	# add any logic wanted for when to send the emails here. (eg errors only)
-	FILESIZE=$(stat -c%s ${OUTPUT_FILE})
-	if [[ ${FILESIZE} -gt 0 ]]
-	then
-			# send mail
-			mail -s "Overdrive Continuous Extract and Reindexing - ${PIKASERVER}" $EMAIL < ${OUTPUT_FILE}
-	fi
-}
-
-function checkForDBCrash() {
-# Pass this function the exit code ($?) of pika java programs.
-# If the exit code is zero that indicates that the pika database is down or unreachable,
-# so we will pause our operations here
-	EXITCODE=$1
-	if [ $EXITCODE -eq 2 ];then
-		sleep 180
-		echo "Received database connection lost error, paused for 180 seconds" >> ${OUTPUT_FILE}
-	fi
-}
+source "/usr/local/vufind-plus/vufind/bash/continuousFunctions.sh"
 
 while true
 do
@@ -35,7 +12,7 @@ do
 	#####
 
 	# Make sure we are not running a Full Record Group/Reindex process
-	hasConflicts=$(checkConflictingProcesses "full_update.sh")
+	hasConflicts=$(checkConflictingProcesses "full_update.sh" 30)
 	#If we did get a conflict, restart the loop to make sure that all tests run
 	if (($? != 0)); then
 		continue
@@ -57,9 +34,17 @@ do
 	# reset the output file each round
 	: > $OUTPUT_FILE;
 
-	#export from overdrive
-	cd /usr/local/vufind-plus/vufind/overdrive_api_extract/
-	nice -n -10 java -server -XX:+UseG1GC -jar overdrive_extract.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+	if [ $USE_SIERRA_API_EXTRACT -eq 1 ]; then
+		#export from sierra (items, holds, and orders)
+		#echo "Starting Sierra Export - `date`" >> ${OUTPUT_FILE}
+		cd /usr/local/vufind-plus/vufind/sierra_export_api/
+		nice -n -10 java -server -XX:+UseG1GC -jar sierra_export_api.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+	else
+		#export from sierra (items, holds, and orders)
+		#echo "Starting Sierra Export - `date`" >> ${OUTPUT_FILE}
+		cd /usr/local/vufind-plus/vufind/sierra_export/
+		nice -n -10 java -server -XX:+UseG1GC -jar sierra_export.jar ${PIKASERVER} >> ${OUTPUT_FILE}
+	fi
 
 	# Pause if another reindexer is running; check in 10 second intervals
 	paused=$(checkConflictingProcesses "reindexer.jar" 10)
@@ -71,7 +56,7 @@ do
 	checkForDBCrash $?
 
 	# send notice of any issues
-	sendEmail
+	sendEmail "Sierra Continuous Extract and Reindexing - ${PIKASERVER}"
 
 		#end block
 done
