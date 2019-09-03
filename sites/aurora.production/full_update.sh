9cfbddf2c980c2febd7c1434fd6504f8701f707f
Chris <chris@marmot.org>
2019-09-03 09:48:57 -0600

    #D-3288 Update min file size check

--- sites/aurora.production/full_update.sh
+++ sites/aurora.production/full_update.sh
@@ -14,7 +14,7 @@ PIKASERVER=aurora.production
 PIKADBNAME=pika
 OUTPUT_FILE="/var/log/vufind-plus/${PIKASERVER}/full_update_output.log"
 
-MINFILE1SIZE=$((267000000))
+MINFILE1SIZE=$((269000000))
 
 # Check if full_update is already running
 PIDFILE="/var/log/vufind-plus/${PIKASERVER}/full_update.pid"
