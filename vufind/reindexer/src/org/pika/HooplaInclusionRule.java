c562abdd7ca355c55b1bffe332258fabbebbe6fc
Pascal Brammeier <>
2019-09-14 19:28:32 -0600

    minor updates to Hoopla indexing

--- vufind/reindexer/src/org/pika/HooplaInclusionRule.java
+++ vufind/reindexer/src/org/pika/HooplaInclusionRule.java
@@ -5,7 +5,6 @@ import org.apache.log4j.Logger;
 public class HooplaInclusionRule {
 	protected Logger logger;
 
-
 	private Long libraryId;
 	private Long locationId;
 	private String kind;  // Hoopla Format
@@ -96,24 +95,24 @@ public class HooplaInclusionRule {
 		return false;
 	}
 
-	public boolean isHooplaTitleIncluded(HooplaExtractInfo hooplaExtractInfo) {
+	public boolean isHooplaTitleExcluded(HooplaExtractInfo hooplaExtractInfo) {
 		if (maxPrice == 0 || hooplaExtractInfo.getPrice() == 0 || hooplaExtractInfo.getPrice() <= maxPrice) {  // include title, if hoopla price isn't set or the max price isn't set or the hoopla price is less than or equal to the max price; and...
 			if (!hooplaExtractInfo.isParentalAdvisory() || !isExcludeParentalAdvisory()) {    // if the title doesn't have a PA warning or we aren't excluding PA warning titles; and...
 				if (!hooplaExtractInfo.isProfanity() || !isExcludeProfanity()) {              // if the title doesn't have profanity or we aren't excluding profanity; and...
 					if (!isIncludeChildrenTitlesOnly() || hooplaExtractInfo.isChildren()) {  // if we aren't limiting to only children's titles or it is a children's title.
-						return true;
-					} else {
-						logger.info("Not include because is not a children's title for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
+						return false;
+					} else if (logger.isDebugEnabled()) {
+						logger.debug("Not including because not a children's title for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
 					}
-				} else {
-					logger.info("Excluding due to profanity for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
+				} else if (logger.isDebugEnabled()) {
+					logger.debug("Excluding due to profanity for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
 				}
-			} else {
-				logger.info("Excluding due to parental advisory for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
+			} else if (logger.isDebugEnabled()) {
+				logger.debug("Excluding due to parental advisory for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
 			}
-		} else {
-			logger.info("Excluding due to price  for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
+		} else if (logger.isDebugEnabled()) {
+			logger.debug("Excluding due to price  for library " + this.libraryId + " or location " + this.locationId + " hoolpa id# " + hooplaExtractInfo.getTitleId() + " - " + hooplaExtractInfo.getTitle());
 		}
-		return false; // Otherwise, exclude the title.
+		return true; // Otherwise, exclude the title.
 	}
 }
