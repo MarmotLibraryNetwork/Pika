c02cd838e20db9337a1176474b3d7875e1db69f3
Chris <chris@marmot.org>
2020-01-22 14:39:56 -0700

    Update php 7 install script

--- install/php74_install.sh
+++ install/php74_install.sh
@@ -4,23 +4,26 @@ echo Stoping contiouns indexing...
 kill $(ps aux | grep [c]ontinuous_reindex | awk '{print $2}')
 
 echo Installing yum-utils...
-yum install yum-utils \
+yum -y install yum-utils \
 || { printf "%b" "Failed to install yum-utils. Exiting.\n" ; exit 1 ; }
+echo
 
 echo Stoping Apache...
 apachectl stop \
 || { printf "%b" "Failed to stop Apache. Please restart Apache after script exits.\nContinuing... \n" ; }
+echo
 
 echo Moving PHP 5.x Apache conf file...
-mv /etc/httpd/conf.modules.d/10-php55-php.conf /etc/httpd/conf.modules.d/10-php55-php.NOconf
+find /etc/httpd/conf.modules.d/ -name "*php55*" -exec mv {} php55.noconf \;
 
 echo Enabling Remi PHP 7.4 repo...
 yum-config-manager --enable remi-php74 \
 || { printf "%b" "Failed to enable Remi PHP 7.4 repo. Exiting.\n" ; exit 1 ; }
+echo
 
 echo Updating PHP packages...
-yum update \
-&& yum install -y php74 php \
+yum update
+yum -y install php74 php \
 php74-php-pecl-memcached php-pecl-memcached \
 php74-php-pecl-memcache php-pecl-memcache \
 php74-php-pecl-mysql php-mysql \
@@ -50,13 +53,27 @@ echo Installing Composer packages...
 cd /usr/local/vufind-plus/install \
 && composer update \
 || { printf "%b" "Failed to install Composer packages. Exiting.\n" ; exit 1 ; }
+echo Composer packages installed.
+echo
 
-echo Copying DataObject to vendor directory
+echo Copying DataObject to vendor directory...
 cp -r /usr/local/vufind-plus/install/PEAR/DB/* /usr/share/composer/vendor/pear/db/DB \
 || { printf "%b" "Failed to copy DataObject. Please check that directory exists.\nExiting.\n" ; exit 1 ; }
+
 cd /usr/share/composer/vendor/pear/db/DB
-chmod -R 655 DataObject
-chmod 644 DataObject.php
+chmod -R 655 DataObject && \
+chmod 655 DataObject.php
 
+echo Starting Apache...
+apachectl start \
+|| { printf "%b" "Failed to start Apache. Please restart when script exits." ; }
 
+echo Restarting memcached...
+systemctl stop memcached && systemctl start memcached
+echo
 echo PHP 7 upgrade complete!
+echo
+echo Next steps:
+echo - Modify config.pwd.ini and replace \"mysql\" in PHP database connection
+echo   string with \"mysqli\"
+
