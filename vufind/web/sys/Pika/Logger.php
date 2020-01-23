bdda178f09f928e555bdcdc93d110e0647bd0b88
Chris <chris@marmot.org>
2020-01-23 14:43:11 -0700

    Add copyright and doc

--- vufind/web/sys/Pika/Logger.php
+++ vufind/web/sys/Pika/Logger.php
@@ -1,21 +1,43 @@
 <?php
 /**
+ * Pika Discovery Layer
+ * Copyright (C) 2020  Marmot Library Network
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/**
+ * Pika Logger
  *
  * @category Pika
+ * @package  Logger
  * @author   Chris Froese
- *
- *
  */
 namespace Pika;
 
 use Monolog\Handler\BrowserConsoleHandler;
 use \Monolog\Logger as MonoLogger;
 use \Monolog\Handler\StreamHandler;
-use \Monolog\Handler\PHPConsoleHandler;
 use \Monolog\ErrorHandler;
 
 class Logger extends MonoLogger {
 
+	/**
+	 * Logger constructor.
+	 * @param string    $name
+	 * @param bool      $registerErrorHandler
+	 * @throws \Exception
+	 */
 	public function __construct($name, $registerErrorHandler = false) {
 		parent::__construct($name);
 		global $configArray;
