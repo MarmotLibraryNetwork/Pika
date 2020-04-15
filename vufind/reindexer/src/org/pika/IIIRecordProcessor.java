f0e20be10ef7a610e765ffbee6aec12d897aaf29
Pascal Brammeier <>
2020-04-15 09:59:06 -0600

    fix errors for language indexing D-3625
    https://marmot.myjetbrains.com/youtrack/issue/D-3625

--- vufind/reindexer/src/org/pika/IIIRecordProcessor.java
+++ vufind/reindexer/src/org/pika/IIIRecordProcessor.java
@@ -521,7 +521,7 @@ abstract class IIIRecordProcessor extends IlsRecordProcessor{
 								// multiple language codes can be smashed together in a single subfield
 								// eg. 041	0		|d latger|e engfregerlat|h gerlat
 								// eg. 041	0		|d engyidfrespaapaund|e engyidfrespaapa|g eng
-								String code         = languageCode.substring(0, 3);
+								String code = languageCode.length() > 3 ? languageCode.substring(0, 3) : languageCode;
 								languageName = indexer.translateSystemValue("language", code, "041" + subfield + " " + identifier);
 								if (primaryLanguage == null && !languageName.equals(code.trim())) {
 									primaryLanguage = languageName;
@@ -557,7 +557,7 @@ abstract class IIIRecordProcessor extends IlsRecordProcessor{
 							languageCode = languageSubfield.getData();
 							do {
 								// multiple language codes can be smashed together in a single subfield
-								String code         = languageCode.substring(0, 3);
+								String code = languageCode.length() > 3 ? languageCode.substring(0, 3) : languageCode;
 								languageName = indexer.translateSystemValue("language", code, "041" + subfield + " " + identifier);
 								if (!languageName.equals(code.trim())) {
 									translationsNames.add(languageName);
