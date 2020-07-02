6cb30112321576cb0278daac1b4a036921c65a84
Pascal Brammeier <>
2020-07-02 11:56:53 -0600

    Update PreferredGroupingAuthors.php D-3613
    https://marmot.myjetbrains.com/youtrack/issue/D-3613

--- vufind/web/services/Admin/PreferredGroupingAuthors.php
+++ vufind/web/services/Admin/PreferredGroupingAuthors.php
@@ -81,7 +81,8 @@ class Admin_PreferredGroupingAuthors extends ObjectEditor {
 	}
 
 	function getInstructions(){
-		return '<p class="alert alert-warning">Note: The <em>Preferred Grouping Author</em> will replace <strong>every</strong> instance of the <em>Source Grouping Author</em> for <u><strong>any</strong> grouped work</u> with that Grouping Author, not just for a single grouped work of interest.</p>';
+		return '<p>For more information about preferred grouping authors, see the <a href="https://docs.google.com/document/d/17OBmsE3ak-FUa5dVzcOtnJAeWypWa--aYzYZsrdliXk">online documentation</a>.</p><br>' .
+		  '<blockquote class="alert-warning">Note: The <em>Preferred Grouping Author</em> will replace <strong>every</strong> instance of the <em>Source Grouping Author</em> for <u><strong>any</strong> grouped work</u> with that Grouping Author, not just for a single grouped work of interest.</blockquote>';
 	}
 
 }
