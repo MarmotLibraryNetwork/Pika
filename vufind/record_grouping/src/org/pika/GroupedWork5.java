055144acfc3c69d2ea76dccb1ec533d3b1e57329
Pascal Brammeier <>
2020-07-17 09:55:45 -0600

    subtitle clean up correction

--- vufind/record_grouping/src/org/pika/GroupedWork5.java
+++ vufind/record_grouping/src/org/pika/GroupedWork5.java
@@ -169,7 +169,7 @@ public class GroupedWork5 extends GroupedWorkBase implements Cloneable {
 	final private static Pattern consecutiveSpaceStrip         = Pattern.compile("\\s{2,}");
 	final private static Pattern bracketedCharacterStrip       = Pattern.compile("\\[(.*?)\\]");
 	final private static Pattern sortTrimmingPattern           = Pattern.compile("(?i)^(?:(?:a|an|the|el|la|las|\"|')\\s)(.*)$");
-	final private static Pattern commonSubtitlesSimplePattern  = Pattern.compile("(\\sby\\s\\w+\\s\\w+|a novel of .*|stories|an autobiography|a biography|a memoir in books|poems|the movie|large print|the graphic novel|graphic novel|magazine|audio cd|book club kit|with illustrations|playaway view|playaway|book \\d+|the original classic edition|classic edition|a novel)$");
+	final private static Pattern commonSubtitlesSimplePattern  = Pattern.compile("(\\s(by\\s\\w+\\s\\w+|a novel of .*|stories|an autobiography|a biography|a memoir in books|poems|the movie|large print|the graphic novel|graphic novel|magazine|audio cd|book club kit|with illustrations|playaway view|playaway|book \\d+|the original classic edition|classic edition|a novel))$");
 	final private static Pattern commonSubtitlesComplexPattern = Pattern.compile("((a|una)\\s(.*)novel(a|la)?|a(.*)memoir|a(.*)mystery|a(.*)thriller|by\\s\\w+\\s\\w+|an? .* story|a .*\\s?book|[\\w\\s]+series book \\d+|the[\\w\\s]+chronicles book \\d+|[\\w\\s]+trilogy book \\d+)$");
 	final private static Pattern editionRemovalPattern         = Pattern.compile("(first|second|third|fourth|fifth|sixth|seventh|eighth|ninth|tenth|revised|\\d+\\S*)\\s+(edition|ed|ed\\.|update|anniversary edition|comprehensive edition|anniversary commemorative edition)");
 	final private static Pattern firstPattern                  = Pattern.compile("1st");
