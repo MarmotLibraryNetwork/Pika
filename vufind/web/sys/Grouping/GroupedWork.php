a762d2658ac186d4b99c388b3ad2f3870475b606
Pascal Brammeier <>
2020-09-23 16:26:26 -0600

    clicking force regrouping to cause the work to be regrouped immediately D-3296
    https://marmot.myjetbrains.com/youtrack/issue/D-3296

--- vufind/web/sys/Grouping/GroupedWork.php
+++ vufind/web/sys/Grouping/GroupedWork.php
@@ -40,43 +40,65 @@ class GroupedWork extends DB_DataObject {
 		return preg_match('/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i', $id) === 1;
 	}
 
-	function forceRegrouping() {
+//	function forceRegrouping() {
+////		require_once ROOT_DIR . '/RecordDrivers/GroupedWorkDriver.php';
+//		if (!empty($this->id)) {
+//			$numRecordsMarked = 0;
+//			require_once ROOT_DIR . '/sys/Grouping/GroupedWorkPrimaryIdentifier.php';
+//			$groupedWorkPrimaryIdentifier                  = new GroupedWorkPrimaryIdentifier();
+//			$groupedWorkPrimaryIdentifier->grouped_work_id = $this->id;
+//			$groupedWorkPrimaryIdentifier->find();
+//			//Get a list of all primary identifiers and mark the checksum as null.
+//			while ($groupedWorkPrimaryIdentifier->fetch()) {
+//				if ($groupedWorkPrimaryIdentifier->type == 'overdrive') {
+//					//For OverDrive titles, just need to set dateUpdated to now.
+//					require_once ROOT_DIR . '/sys/OverDrive/OverDriveAPIProduct.php';
+//					$overDriveProduct              = new OverDriveAPIProduct();
+//					$overDriveProduct->overdriveId = $groupedWorkPrimaryIdentifier->identifier;
+//					if ($overDriveProduct->find(true)) {
+//						$overDriveProduct->dateUpdated = time();
+//						 if ($overDriveProduct->update()) {
+//						 	$numRecordsMarked++;
+//						 }
+//					}
+//				} else {
+//					//Mark the checksum as 0.
+//					require_once ROOT_DIR . '/sys/Extracting/IlsMarcChecksum.php';
+//					$ilsMarcChecksum         = new IlsMarcChecksum();
+//					$ilsMarcChecksum->ilsId  = $groupedWorkPrimaryIdentifier->identifier;
+//					$ilsMarcChecksum->source = $groupedWorkPrimaryIdentifier->type;
+//					if ($ilsMarcChecksum->find(true)) {
+//						$ilsMarcChecksum->checksum = 0;
+//						if($ilsMarcChecksum->update()) {
+//							$numRecordsMarked++;
+//						}
+//					}
+//				}
+//			}
+//			return $numRecordsMarked;
+//		}
+//		return false;
+//	}
+
+	function forceRegrouping(){
 //		require_once ROOT_DIR . '/RecordDrivers/GroupedWorkDriver.php';
-		if (!empty($this->id)) {
-			$numRecordsMarked = 0;
-			require_once ROOT_DIR . '/sys/Grouping/GroupedWorkPrimaryIdentifier.php';
-			$groupedWorkPrimaryIdentifier                  = new GroupedWorkPrimaryIdentifier();
-			$groupedWorkPrimaryIdentifier->grouped_work_id = $this->id;
-			$groupedWorkPrimaryIdentifier->find();
-			//Get a list of all primary identifiers and mark the checksum as null.
-			while ($groupedWorkPrimaryIdentifier->fetch()) {
-				if ($groupedWorkPrimaryIdentifier->type == 'overdrive') {
-					//For OverDrive titles, just need to set dateUpdated to now.
-					require_once ROOT_DIR . '/sys/OverDrive/OverDriveAPIProduct.php';
-					$overDriveProduct              = new OverDriveAPIProduct();
-					$overDriveProduct->overdriveId = $groupedWorkPrimaryIdentifier->identifier;
-					if ($overDriveProduct->find(true)) {
-						$overDriveProduct->dateUpdated = time();
-						 if ($overDriveProduct->update()) {
-						 	$numRecordsMarked++;
-						 }
-					}
-				} else {
-					//Mark the checksum as 0.
-					require_once ROOT_DIR . '/sys/Extracting/IlsMarcChecksum.php';
-					$ilsMarcChecksum         = new IlsMarcChecksum();
-					$ilsMarcChecksum->ilsId  = $groupedWorkPrimaryIdentifier->identifier;
-					$ilsMarcChecksum->source = $groupedWorkPrimaryIdentifier->type;
-					if ($ilsMarcChecksum->find(true)) {
-						$ilsMarcChecksum->checksum = 0;
-						if($ilsMarcChecksum->update()) {
-							$numRecordsMarked++;
-						}
-					}
-				}
+		if (!empty($this->permanent_id)){
+
+			// Get site name from covers directory
+			global $configArray;
+			$partParts = explode("/", $configArray['Site']['coverPath']);
+			$siteName  = $partParts[count($partParts) - 2];
+
+			$localPath          = $configArray['Site']['local'];
+			$recordGroupingPath = realpath("$localPath/../record_grouping/");
+			$commandToRun       = "java -jar $recordGroupingPath/record_grouping.jar $siteName singleWork {$this->permanent_id}";
+			$result             = shell_exec($commandToRun);
+			$result             = json_decode($result);
+			if (!empty($result['success'])){
+				return true;
 			}
-			return $numRecordsMarked;
 		}
+
 		return false;
 	}
 
