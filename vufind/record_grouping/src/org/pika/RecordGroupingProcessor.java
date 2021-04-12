271d20dbc4ef9b496becf2cc7f31dd24f1721523
Pascal Brammeier <pascal@marmot.org>
2021-04-12 16:15:23 -0600

    Automatic econtent suppression from grouping for ILS OverDrive records

--- vufind/record_grouping/src/org/pika/RecordGroupingProcessor.java
+++ vufind/record_grouping/src/org/pika/RecordGroupingProcessor.java
@@ -145,7 +145,7 @@ class RecordGroupingProcessor {
 
 	//	private static Pattern overdrivePattern = Pattern.compile("(?i)^http://.*?lib\\.overdrive\\.com/ContentDetails\\.htm\\?id=[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}$");
 	//above pattern not strictly valid because urls don't have to contain the lib.overdrive.com
-	private static Pattern overdrivePattern = Pattern.compile("(?i)^http://.*?/ContentDetails\\.htm\\?id=[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}$|^http://link\\.overdrive\\.com");
+	private static final Pattern overdrivePattern = Pattern.compile("(?i)^http://.*?/ContentDetails\\.htm\\?id=[\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12}$|^http://link\\.overdrive\\.com.*");
 
 	RecordIdentifier getPrimaryIdentifierFromMarcRecord(Record marcRecord, String recordSource, boolean doAutomaticEcontentSuppression) {
 		RecordIdentifier    identifier         = null;
