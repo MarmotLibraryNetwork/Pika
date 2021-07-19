923ec2b4678e0a468b3e1f237a3fd13143a5f61c
Pascal Brammeier <pascal@marmot.org>
2021-07-19 10:17:55 -0600

    Update NYTList.java D-2727
    https://marmot.myjetbrains.com/youtrack/issue/D-2727

--- vufind/cron/src/org/pika/NYTList.java
+++ vufind/cron/src/org/pika/NYTList.java
@@ -1,9 +1,7 @@
 package org.pika;
 
-import com.sun.tools.internal.xjc.model.CNonElement;
 import org.apache.log4j.Logger;
 import org.ini4j.Profile.Section;
-import org.w3c.dom.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,47 +13,47 @@ public class NYTList implements IProcessHandler {
 
 	@Override
 	public void doCronProcess(String serverName, Section processSettings, Connection pikaConn, Connection econtentConn, CronLogEntry cronEntry, Logger logger, PikaSystemVariables systemVariables) {
-			CronProcessLogEntry processEntry = new CronProcessLogEntry(cronEntry.getLogEntryId(), "NYT Updates");
-			processEntry.saveToDatabase(pikaConn, logger);
-		try{
-			if(!systemVariables.getBooleanValuedVariable("full_reindex_running"))
-			{
-				  String url = "http://" + serverName + ":8080/solr/admin/cores?wt=json";
+		CronProcessLogEntry processEntry = new CronProcessLogEntry(cronEntry.getLogEntryId(), "NYT Updates");
+		processEntry.saveToDatabase(pikaConn, logger);
+		try {
+			final Boolean fullReindexRunning = systemVariables.getBooleanValuedVariable("full_reindex_running");
+			if (fullReindexRunning != null && !fullReindexRunning) {
+				String url = PikaConfigIni.getIniValue("Index", "url") + "/admin/cores?wt=json";
 
-					if(isSolrRunning(url, logger, processEntry))
-					{
-						 addNYTItemsToList("https://" + serverName, logger, processEntry, pikaConn);
-					}
-					else{
-						logger.error("Solr Down");
-					}
+				if (isSolrRunning(url, logger, processEntry)) {
+					addNYTItemsToList(PikaConfigIni.getIniValue("Site", "url"), logger, processEntry, pikaConn);
+				} else {
+					final String message = "Solr Down; Not Updating NY Times User Lists";
+					logger.error(message);
+					processEntry.addNote(message);
+				}
+			} else {
+				final String message = "Full Reindex Running; Not Updating NY Times User Lists";
+				logger.error(message);
+				processEntry.addNote(message);
 			}
-			else{
-				logger.error("Full Reindex Running");
-			}
+		} catch (Exception e) {
+			logger.error(e);
 		}
-		catch(Exception e )
-		{
-				logger.error(e);
-		}
+		processEntry.saveToDatabase(pikaConn, logger);
 	}
 
 	public boolean isSolrRunning(String url, Logger logger, CronProcessLogEntry processEntry) throws MalformedURLException {
 
 			URL solrLocation = new URL(url);
 		try {
-			Scanner scan = new Scanner(solrLocation.openStream());
-			String str = new String();
-			while (scan.hasNext())
-				str += scan.nextLine();
-			scan.close();
+			StringBuilder str;
+			try (Scanner scan = new Scanner(solrLocation.openStream())) {
+				str = new StringBuilder();
+				while (scan.hasNext())
+					str.append(scan.nextLine());
+			}
 
-			JSONObject obj = new JSONObject(str);
-		  JSONObject statusObject = obj.getJSONObject("status");
+			JSONObject obj           = new JSONObject(str.toString());
+		  JSONObject statusObject  = obj.getJSONObject("status");
 			JSONObject groupedObject = statusObject.getJSONObject("grouped");
-			Integer uptime = Integer.parseInt(groupedObject.get("uptime").toString());
-			if (uptime > 0 && uptime < 999000000)
-			{
+			int        uptime        = Integer.parseInt(groupedObject.get("uptime").toString());
+			if (uptime > 0 && uptime < 999000000) {
 				return true;
 			}
 		} catch (Exception e) {
@@ -65,47 +63,44 @@ public class NYTList implements IProcessHandler {
 		return false;
 	}
 
-	public void addNYTItemsToList(String serverName, Logger logger, CronProcessLogEntry processEntry, Connection pikaConn ) throws MalformedURLException {
-		String url = serverName + "/API/ListAPI?method=getAvailableListsFromNYT";
-		URL apiLocation = new URL(url);
-		try{
-			Scanner scan = new Scanner(apiLocation.openStream());
-			String str = new String();
-			while (scan.hasNext())
-			  str += scan.nextLine();
-			scan.close();
-			JSONObject obj = new JSONObject(str);
-			JSONObject result = obj.getJSONObject("result");
-			JSONArray results = new JSONArray();
-			results = result.getJSONArray("results");
-			for(int i = 0; i < results.length(); i++)
-			{
-				JSONObject newresult = (JSONObject) results.get(i);
-
-				String encoded_list_name = newresult.get("list_name_encoded").toString();
-				String updateUrl = serverName + "/API/ListAPI?method=createUserListFromNYT&listToUpdate=" + encoded_list_name;
-				URL updateLocation = new URL(updateUrl);
-				Scanner updateScan = new Scanner(updateLocation.openStream());
-				String updateStr = "";
-				while (updateScan.hasNext()) {
-					updateStr += updateScan.nextLine();
+	public void addNYTItemsToList(String pikaSiteURL, Logger logger, CronProcessLogEntry processEntry, Connection pikaConn ) throws MalformedURLException {
+		String url         = pikaSiteURL + "/API/ListAPI?method=getAvailableListsFromNYT";
+		URL    apiLocation = new URL(url);
+		try {
+			StringBuilder str;
+			try (Scanner scan = new Scanner(apiLocation.openStream())) {
+				str = new StringBuilder();
+				while (scan.hasNext()) {
+					str.append(scan.nextLine());
 				}
-				JSONObject updateStatus = new JSONObject(updateStr);
-
-				JSONObject resultJSON =	updateStatus.getJSONObject("result");
-				if(resultJSON.getBoolean("success"))
-				{
-						processEntry.addNote("Updated List: " + encoded_list_name);
-						processEntry.saveToDatabase(pikaConn, logger);
-
-				}
-				else {
-					processEntry.addNote("Could not update list: " + encoded_list_name);
-				}
-				updateScan.close();
 			}
-		}catch(Exception e)
-		{
+			JSONObject obj     = new JSONObject(str.toString());
+			JSONObject result  = obj.getJSONObject("result");
+			JSONArray  results = result.getJSONArray("results");
+			for (int i = 0; i < results.length(); i++) {
+				JSONObject    newResult         = (JSONObject) results.get(i);
+				String        encoded_list_name = newResult.get("list_name_encoded").toString();
+				String        updateUrl         = pikaSiteURL + "/API/ListAPI?method=createUserListFromNYT&listToUpdate=" + encoded_list_name;
+				URL           updateLocation    = new URL(updateUrl);
+				try (Scanner updateScan = new Scanner(updateLocation.openStream())) {
+					StringBuilder updateStr = new StringBuilder();
+					while (updateScan.hasNext()) {
+						updateStr.append(updateScan.nextLine());
+					}
+					JSONObject updateStatus = new JSONObject(updateStr.toString());
+					JSONObject resultJSON   = updateStatus.getJSONObject("result");
+					if (resultJSON.getBoolean("success")) {
+						processEntry.addNote("Updated List: " + encoded_list_name);
+					} else {
+						processEntry.addNote("Could not update list: " + encoded_list_name);
+					}
+					processEntry.saveToDatabase(pikaConn, logger);
+				} catch (Exception e){
+					logger.error("Error trying to update NY Times list " + encoded_list_name, e);
+					// Caught exception, now try to build other lists
+				}
+			}
+		} catch (Exception e) {
 			logger.error("Cannot reach Solr server or server down");
 		}
 	}
