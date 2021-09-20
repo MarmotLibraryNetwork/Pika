2c1e9fe233b08abc592970a632f25e931341e73c
Pascal Brammeier <pascal@marmot.org>
2021-09-20 09:36:55 -0600

    Clean up robots.php

--- vufind/web/robots.php
+++ vufind/web/robots.php
@@ -29,32 +29,28 @@
 require_once 'bootstrap.php';
 global $configArray;
 global $library;
-global $serverName;
-global $interface;
 if ($configArray['Site']['isProduction']){
-	echo(@file_get_contents('robots.txt'));
+	echo @file_get_contents('robots.txt');
+	$url = !empty($library->catalogUrl) ? $library->catalogUrl : $configArray['Site']['ur'];
 
-	if ($library != null){
+	if (!empty($library->subdomain)){
 		$subdomain = $library->subdomain;
+		$fileName  = $subdomain . '.xml';
 
 		/*
 		 * sitemap: <sitemap_url>
 		 * */
-		$file = 'robots.txt';
-		// Open the file to get existing content
-		$current = file_get_contents($file);
 
-		$fileName = $subdomain . '.xml';
-		$siteMap_Url = 'Sitemap: ' . $configArray['Site']['url'] . '/sitemaps/' .$fileName;
-		// Append a new line char
-		echo "\n";
-		//Append the site map index file url
-		echo $siteMap_Url . "\n";
+		echo <<<BLOCK
 
+Sitemap: $url/sitemaps/$fileName
+sitemap: $url/sitemaps/$fileName
+
+
+BLOCK;
 		//Google may want this with a lower case sitemap even though they specify capitalized.  Provide both.
-		$siteMap_Url2 = 'sitemap: ' . $configArray['Site']['url'] . '/sitemaps/' .$fileName;
-		echo $siteMap_Url2 . "\n";
+
 	}
-}else {
-	echo("User-agent: *\r\nDisallow: /\r\n");
+}else{
+	echo "User-agent: *\r\nDisallow: /\r\n";
 }
