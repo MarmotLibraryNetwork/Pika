018c708a2c783cb86bc75f8903c3bdf213ab25eb
Pascal Brammeier <pascal@marmot.org>
2021-10-04 17:01:18 -0600

    Reduce popularity boosting D-4009
    https://marmot.myjetbrains.com/youtrack/issue/D-4009

--- vufind/web/sys/Search/Solr.php
+++ vufind/web/sys/Search/Solr.php
@@ -832,7 +832,30 @@ class Solr implements IndexEngine {
 			$boostFactors[] = "language_boost_{$solrScope}";
 		}
 
-		$boostFactors[] = (!empty($searchLibrary->applyNumberOfHoldingsBoost)) ? 'product(sum(popularity,1),format_boost)' : 'format_boost';
+
+//		$boostFactors[] = (!empty($searchLibrary->applyNumberOfHoldingsBoost)) ? 'product(sum(popularity,1),format_boost)' : 'format_boost';
+
+		if ((!empty($searchLibrary->applyNumberOfHoldingsBoost))){
+			$boostFactors[] = 'sum(popularity,1)';
+		}
+		$boostFactors[] = 'format_boost';
+
+		// popularity is indexed as zero or greater, but to apply to boosting we want it to be a value of 1 or greater
+		// hence sum(popularity,1)
+
+		//For physical records, popularity is determined by the checkouts for each item with this formula :
+		//  year-to-date checkouts
+		//  plus half of last year's checkouts
+		//  plus a tenth of total checkouts minus last year's checkouts and minus the year-to-date checkouts
+		// Or a value of one, if the calculation above is zero
+		// So a record's popularity is the sum of this calculation for every item
+		//  plus twice the number of holds there are on the record
+
+		// For order records, the popularity is based on the number of copies for each order record
+
+		// For Overdrive, the popularity is the overdrive metadata measure popularity divided by 500; or 1 if it is less than 500
+
+		// For Sideloads and Hoopla, the popularity is the number of bibs
 
 		// Add rating as part of the ranking, normalize so ratings of less that 2.5 are below unrated entries.
 		$boostFactors[] = 'sum(rating,1)';
