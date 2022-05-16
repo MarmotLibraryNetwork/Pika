77bbf9dec309949d80b4fb40020809b44fc5f857
Chris <chris@marmot.org>
2022-05-16 14:07:10 -0600

    code formatting

--- vufind/web/sys/Pika/PatronDrivers/HorizonROA.php
+++ vufind/web/sys/Pika/PatronDrivers/HorizonROA.php
@@ -80,7 +80,6 @@ abstract class HorizonROA implements \DriverInterface {
 		return [$lastName, $firstName];
 	}
 
-
 	public function getWebServiceURL(){
 		if (empty($this->webServiceURL)){
 			$webServiceURL = null;
@@ -175,7 +174,7 @@ abstract class HorizonROA implements \DriverInterface {
 			$session           = [false, false, false];
 			$loginUserUrl      =  '/user/patron/login';
 			$params            = [
-				'barcode'    => $barcode,
+				'barcode'  => $barcode,
 				'password' => $password,
 			];
 			$loginUserResponse = $this->getWebServiceResponse($loginUserUrl, $params);
@@ -210,7 +209,6 @@ abstract class HorizonROA implements \DriverInterface {
 			return $sessionToken;
 		}
 	}
-
 
 	/**
 	 * @param $barcode
@@ -262,9 +260,9 @@ abstract class HorizonROA implements \DriverInterface {
 			$acountInfoLookupURL =  '/v1/user/patron/key/' . $horizonRoaUserID
 			. '?includeFields=displayName,privilegeExpiresDate,primaryAddress,primaryPhone,library,patronType'
 			. ',holdRecordList,circRecordList,blockList'
-			. ",estimatedOverdueAmount"
+			. ",estimatedOverdueAmount";
 			// Note that {*} notation doesn't work for Horizon ROA yet
-		;
+
 			//TODO: see what default fields is now
 
 			// phoneList is for texting notification preferences
