9b46bc47b1427837ee7495b09ad3a5d1817aad28
Pascal Brammeier <pascal@marmot.org>
2022-08-08 16:27:27 -0600

    Export user lists of archive objects to excel D-3607
    https://marmot.myjetbrains.com/youtrack/issue/D-3607

--- vufind/web/services/MyAccount/MyList.php
+++ vufind/web/services/MyAccount/MyList.php
@@ -127,7 +127,7 @@ class MyAccount_MyList extends MyAccount {
 							session_commit();
 							break;
 						case 'exportToExcel':
-							$this->exportToExcel($list);
+							self::exportToExcel($list);
 							break;
 					}
 				}elseif (isset($_REQUEST['delete'])){
@@ -256,7 +256,7 @@ class MyAccount_MyList extends MyAccount {
 	 * @throws Exception
 	 *
 	 */
-	public function exportToExcel(UserList $list){
+	static public function exportToExcel(UserList $list){
 
 		global $interface;
 		$interface->assign('favList', $list);
@@ -267,7 +267,7 @@ class MyAccount_MyList extends MyAccount {
 			$userCanEdit = $listUser->canEditList($list);
 		}
 		$favList   = new FavoriteHandler($list, $userCanEdit);
-		$favorites = $favList->getTitles($list->id, true);
+		$favorites = $favList->getTitles($list->id);
 
 
 		//PHPEXCEL
@@ -278,48 +278,73 @@ class MyAccount_MyList extends MyAccount {
 		$gitBranch = $interface->getVariable('gitBranch');
 		$objPHPExcel->getProperties()->setCreator('Pika ' . $gitBranch)
 			->setLastModifiedBy('Pika ' . $gitBranch)
-			->setTitle("Office 2007 XLSX Document")
-			->setSubject("Office 2007 XLSX Document")
-			->setDescription("Office 2007 XLSX, generated using PHP.")
-			->setKeywords("office 2007 openxml php")
-			->setCategory("List Items");
+			->setTitle('Office 2007 XLSX Document')
+			->setSubject('Office 2007 XLSX Document')
+			->setDescription('Office 2007 XLSX, generated using PHP.')
+			->setKeywords('office 2007 openxml php')
+			->setCategory('List Items');
 		// Set Labeled Cell Values
 		$entries   = $list->getListTitles();
 		$itemEntry = [];
 
 		//create subarray with notes and dateAdded
 		foreach ($entries as $entry){
-			$itemEntry [$entry->groupedWorkPermanentId] = ["notes" => $entry->notes, "dateAdded" => $entry->dateAdded, "weight" => $entry->weight];
+			$itemEntry[$entry->groupedWorkPermanentId] = [
+				'notes'     => $entry->notes,
+				'dateAdded' => $entry->dateAdded,
+				'weight'    => $entry->weight
+			];
 		}
 
 		//create array including all data
-		$typeString = "format_category_" . $GLOBALS['solrScope'];
 		$itemArray  = [];
 		foreach ($favorites as $listItem){
-			$title = "";
-			if (!empty($listItem['title_display'])){
+			$recordID = $listItem['id'] ?? $listItem['PID'];
+			$isArchive = isset($listItem['PID']);
+			$isCatalog = isset($listItem['id']);
+
+			$title = '';
+			if ($isCatalog && !empty($listItem['title_display'])){
 				$title = $listItem['title_display'];
+			} elseif ($isArchive && !empty($listItem['fgs_label_s'])){
+				$title = $listItem['fgs_label_s'];
 			}
-			$author = "";
+			$author = '';
 			if (!empty($listItem['author_display'])){
 				$author = $listItem['author_display'];
 			}
-			$recordType = "";
-			if (!empty($listItem['recordtype'])){
-				$recordType = $listItem['recordtype'];
-			}
-			$type = "";
-			if (isset($listItem[$typeString])){
+
+			//TODO: recordType needed?
+//			$recordType = '';
+//			if (!empty($listItem['recordtype'])){
+//				$recordType = $listItem['recordtype'];
+//			} elseif ($isArchive){
+//				$recordType = 'archive';
+//			}
+
+			$type = '';
+			$typeString = 'format_category_' . $GLOBALS['solrScope'];
+			if ($isCatalog && isset($listItem[$typeString])){
 				$type = $listItem[$typeString][0];
+			} elseif ($isArchive && isset($listItem['mods_genre_s'])){
+				$type = $listItem['mods_genre_s'];
 			}
 
-			$recordID = $listItem['id'];
 
-			$favoriteItem = ["Title" => $title, "Author" => $author, "recordType" => $recordType, "Type" => $type, "recordID" => $recordID, "Date" => $itemEntry[$recordID]['dateAdded'], "Notes" => $itemEntry[$recordID]['notes'], "Weight" => $itemEntry[$recordID]['weight']];
+			$favoriteItem = [
+				'Title'      => $title,
+				'Author'     => $author,
+//				'recordType' => $recordType,
+				'Type'       => $type,
+				'recordID'   => $recordID,
+				'Date'       => $itemEntry[$recordID]['dateAdded'],
+				'Notes'      => $itemEntry[$recordID]['notes'],
+				'Weight'     => $itemEntry[$recordID]['weight']
+			];
 			array_push($itemArray, $favoriteItem);
 		}
 
-		$this->SortByValue($itemArray, $favList->getSort());
+		self::SortByValue($itemArray, $favList->getSort());
 
 		$objPHPExcel->setActiveSheetIndex(0)
 			->setCellValue('A1', $list->title)
@@ -328,7 +353,8 @@ class MyAccount_MyList extends MyAccount {
 			->setCellValue('B3', 'Author')
 			->setCellValue('C3', 'Notes')
 			->setCellValue('D3', 'Material Type')
-			->setCellValue('E3', 'Date Added');
+			->setCellValue('E3', 'Date Added')
+			->setCellValue('F3', 'Id');
 
 
 		$a = 4;
@@ -338,7 +364,8 @@ class MyAccount_MyList extends MyAccount {
 				->setCellValue('B' . $a, $listItem['Author'])
 				->setCellValue('C' . $a, $listItem['Notes'])
 				->setCellValue('D' . $a, $listItem['Type'])
-				->setCellValue('E' . $a, date('m/d/Y g:i a', $listItem['Date']));
+				->setCellValue('E' . $a, date('m/d/Y g:i a', $listItem['Date']))
+				->setCellValue('F' . $a, $listItem['recordID']);
 			$a++;
 
 		}
@@ -378,8 +405,7 @@ class MyAccount_MyList extends MyAccount {
 	 * @param string $field
 	 * @return array
 	 */
-	private function SortByValue(array &$array, string $field){
-
+	static private function SortByValue(array &$array, string $field){
 		switch ($field){
 			case "title":
 				$key = "Title";
@@ -396,7 +422,6 @@ class MyAccount_MyList extends MyAccount {
 				break;
 			default:
 				return $array;
-				break;
 		}
 		$sorter = [];
 		$ret    = [];
@@ -408,22 +433,18 @@ class MyAccount_MyList extends MyAccount {
 				if (is_null($val)){
 					$val = 0;
 				}
-
 			}else{
 				$val = $va[$key];
 			}
-
 		}
 		if ($field == "recentlyAdded"){
 			arsort($sorter);
 		}else{
-
 			asort($sorter);
 		}
 		foreach ($sorter as $ii => $val){
 			$ret[$ii] = $array[$ii];
 		}
 		$array = $ret;
-
 	}
 }
