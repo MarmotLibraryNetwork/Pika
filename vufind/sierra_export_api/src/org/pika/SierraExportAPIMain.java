60cd1b50155f4b4cf5fcf6c321e6bfaed03c54b3
Pascal Brammeier <pascal@marmot.org>
2023-08-14 15:05:59 -0600

    Fix reading diacritic UTF8 characters in Sierra Extraction D-4555
    https://marmot.myjetbrains.com/youtrack/issue/D-4555/

--- vufind/sierra_export_api/src/org/pika/SierraExportAPIMain.java
+++ vufind/sierra_export_api/src/org/pika/SierraExportAPIMain.java
@@ -16,7 +16,6 @@ package org.pika;
 
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
-import org.apache.commons.codec.binary.Base64;
 // Import log4j classes.
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -2091,7 +2090,7 @@ public class SierraExportAPIMain {
 			URL    emptyIndexURL = new URL(apiBaseUrl + "/token");
 			String clientKey     = PikaConfigIni.getIniValue("Catalog", "clientKey");
 			String clientSecret  = PikaConfigIni.getIniValue("Catalog", "clientSecret");
-			String encoded       = Base64.encodeBase64String((clientKey + ":" + clientSecret).getBytes());
+			String encoded       = new String(Base64.getEncoder().encode((clientKey + ":" + clientSecret).getBytes()));
 
 			conn = (HttpURLConnection) emptyIndexURL.openConnection();
 			checkForSSLConnection(conn);
@@ -2148,8 +2147,8 @@ public class SierraExportAPIMain {
 
 	private static StringBuilder getTheResponse(InputStream inputStream) {
 		StringBuilder response = new StringBuilder();
-		try (BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream))) {
-//		try (BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))) { //TODO: Is this additional setting needed?
+		try (BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+			// Setting the charset is apparently important sometimes. See D-4555
 			String line;
 			while ((line = rd.readLine()) != null) {
 				response.append(line);
