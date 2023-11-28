16b1448b74dab6ce3ee4ad1e0e615db3ac5a9bb8
C.J. O'Hara <cj@marmot.org>
2023-11-28 09:54:35 -0700

    D-4563 Security Vulnerability:  List page, pagesize, sort variables set from unfiltered $smarty.get
    Params weren't being passed without actions.

--- vufind/web/services/MyAccount/MyList.php
+++ vufind/web/services/MyAccount/MyList.php
@@ -46,6 +46,7 @@ class MyAccount_MyList extends MyAccount {
 		$list       = new UserList();
 		$list->id   = $listId;
 		$listExists = $list->find(true);
+		$params = array();
 		if ($listExists){
 			// Ensure user has privileges to view the list
 			if (!$list->public){
@@ -136,7 +137,7 @@ class MyAccount_MyList extends MyAccount {
 					$list->update();
 				}
 				//Redirect back to avoid having the parameters stay in the URL (keeping both pagesize and current page).
-				$params = [];
+
 				if (!empty($_REQUEST['pagesize']) && is_numeric($_REQUEST['pagesize'])){
 					$params['pagesize'] = $_REQUEST['pagesize'];
 				}
@@ -149,7 +150,7 @@ class MyAccount_MyList extends MyAccount {
 				if (!empty($_REQUEST['filter'])){
 					$params['filter'] = $_REQUEST['filter'];
 				}
-				$interface->assign('params', $params);
+
 				$queryString = empty($params) ? '' : '?' . http_build_query($params);
 				header("Location: /MyAccount/MyList/{$list->id}" . $queryString);
 				die;
@@ -168,11 +169,23 @@ class MyAccount_MyList extends MyAccount {
 			}
 
 			// Send list to template so title/description can be displayed:
-
+			if (!empty($_REQUEST['pagesize']) && is_numeric($_REQUEST['pagesize'])){
+				$params['pagesize'] = $_REQUEST['pagesize'];
+			}
+			if (!empty($_REQUEST['page']) && is_numeric($_REQUEST['page'])){
+				$params['page'] = $_REQUEST['page'];
+			}
+			if (!empty($_REQUEST['sort']) && in_array($_REQUEST['sort'],array('author','title','dateAdded','recentlyAdded','custom'))){
+				$params['sort'] = $_REQUEST['sort'];
+			}
+			if (!empty($_REQUEST['filter'])){
+				$params['filter'] = $_REQUEST['filter'];
+			}
 			$interface->assign('favList', $list);
 //		$shortTitle = $list->title;
 //		$interface->assign('shortPageTitle', $shortTitle);
 			//shortPageTitle does get assigned in the class Action method of display()
+			$interface->assign('params', $params);
 
 			$interface->assign('listSelected', $list->id);
 
