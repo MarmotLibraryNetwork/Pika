2d7d601aab16a5a68815696a7b018cb982da12d3
Chris <chris@marmot.org>
2024-06-10 12:26:10 -0600

    Initial patron driver class for Polaris and clearview

---
+++ vufind/web/sys/Pika/PatronDrivers/Clearview.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * Class Clearview
+ *
+ * Methods needed for completing Clearview Library Network actions in the Polaris ILS
+ *
+ * @category Pika
+ * @package  PatronDrivers
+ * @author   Chris Froese
+ * Date      6-10-2024
+ *
+ */
+
+namespace Pika\PatronDrivers;
+
+class Clearview extends Polaris
+{
+
+}
