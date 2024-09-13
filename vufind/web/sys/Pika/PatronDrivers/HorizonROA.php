06e411e29815d3cc6c208fec42610ecc28da8682
Pascal Brammeier <pascal@marmot.org>
2024-09-13 11:46:19 -0600

    Disable get Record Holds Number look up

--- vufind/web/sys/Pika/PatronDrivers/HorizonROA.php
+++ vufind/web/sys/Pika/PatronDrivers/HorizonROA.php
@@ -522,19 +522,19 @@ abstract class HorizonROA extends PatronDriverInterface implements \DriverInterf
 		// TODO: make ROA call
 		//This uses the standard / REST method to retrieve this information from the ILS.
 		// It isn't an ROA call.
-		global $configArray;
-		if (empty($configArray['Catalog']['offline'])){
-			$lookupTitleInfoUrl      = '/rest/standard/lookupTitleInfo?titleKey=' . $bibId . '&includeItemInfo=false&includeHoldCount=true';
-			$lookupTitleInfoResponse = $this->getWebServiceResponse($lookupTitleInfoUrl);
-			if (!empty($lookupTitleInfoResponse->titleInfo)){
-				if (is_array($lookupTitleInfoResponse->titleInfo) && isset($lookupTitleInfoResponse->titleInfo[0]->holdCount)){
-					return (int)$lookupTitleInfoResponse->titleInfo[0]->holdCount;
-				}elseif (isset($lookupTitleInfoResponse->titleInfo->holdCount)){
-					//TODO: I suspect that this never occurs
-					return (int)$lookupTitleInfoResponse->titleInfo->holdCount;
-				}
-			}
-		}
+//		global $configArray;
+//		if (empty($configArray['Catalog']['offline'])){
+//			$lookupTitleInfoUrl      = '/rest/standard/lookupTitleInfo?titleKey=' . $bibId . '&includeItemInfo=false&includeHoldCount=true';
+//			$lookupTitleInfoResponse = $this->getWebServiceResponse($lookupTitleInfoUrl);
+//			if (!empty($lookupTitleInfoResponse->titleInfo)){
+//				if (is_array($lookupTitleInfoResponse->titleInfo) && isset($lookupTitleInfoResponse->titleInfo[0]->holdCount)){
+//					return (int)$lookupTitleInfoResponse->titleInfo[0]->holdCount;
+//				}elseif (isset($lookupTitleInfoResponse->titleInfo->holdCount)){
+//					//TODO: I suspect that this never occurs
+//					return (int)$lookupTitleInfoResponse->titleInfo->holdCount;
+//				}
+//			}
+//		}
 		return false;
 	}
 
