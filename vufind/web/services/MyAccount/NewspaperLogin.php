832cdae46ff2c457cf99e86f52721c242617afa8
Pascal Brammeier <pascal@marmot.org>
2025-02-21 16:39:30 -0700

    D-4987 Correct displaying no access page for Newspaper Login pages
    https://marmot.myjetbrains.com/youtrack/issue/D-4987

--- vufind/web/services/MyAccount/NewspaperLogin.php
+++ vufind/web/services/MyAccount/NewspaperLogin.php
@@ -22,30 +22,43 @@ class NewspaperLogin extends MyAccount {
 
 	public String $newpaperName = 'Newspaper'; // Overwrite this value in the extending class
 	public String $newpaperUrl; // Set this value in the extending class
+
 	function launch(){
-		/** @var Library $library */
-		$library = UserAccount::getUserHomeLibrary();
-		$column = $this->newpaperUrl;
-		//TODO: validate for url structure
-		if (!empty($column) && !empty($library->$column)){
-			header('Location: ' . $library->$column);
-			exit();
-		}else{
-			$this->display('../NewspaperLogin/noAccess.tpl', 'No Access');
+		// Upon successful login redirect to newspaper subscription
+		if (UserAccount::isLoggedIn()){
+			/** @var Library $library */
+			$library = UserAccount::getUserHomeLibrary();
+			$column  = $this->newpaperUrl;
+			if (!empty($column) && !empty($library->$column)){
+				header('Location: ' . $library->$column);
+				exit();
+			} else {
+				// This is for when the user has used an interface that has a subscription URL
+				// (that is different their home library), but the user's home library doesn't have a
+				// subscription URL
+				$this->display('../NewspaperLogin/noAccess.tpl', 'No Access');
+				exit;
+			}
 		}
 	}
 
-
 	public function __construct(){
 		/** @var Library $library */
-		global $library;
-		global $interface;
-		$interface->assign([
-			'newspaperName'        => $this->newpaperName,
-			'newspaperLibraryName' => $library->displayName,
-		]);
-		// Need to set the display name for the information blurb on the login page because the traditional
-		// $librarySystemName template variable is set to the location display name when the location is known.
-		parent::__construct();
+		global $library; // The interface's library
+		$column = $this->newpaperUrl ?? null;
+		if (!empty($column) && !empty($library->$column)) {
+			global $interface;
+			$interface->assign([
+				'newspaperName'        => $this->newpaperName,
+				'newspaperLibraryName' => $library->displayName,
+			]);
+
+			// Need to set the display name for the information blurb on the login page because the traditional
+			// $librarySystemName template variable is set to the location display name when the location is known.
+			parent::__construct();
+		} else {
+			$this->display('../NewspaperLogin/noAccess.tpl', 'No Access');
+			exit;
+		}
 	}
 }
