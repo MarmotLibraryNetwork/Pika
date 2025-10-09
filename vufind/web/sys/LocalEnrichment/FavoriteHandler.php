313978db8fae18c4c514ba2c4a904a9e27646124
Pascal Brammeier <pascal@marmot.org>
2025-10-09 13:59:22 -0600

    Correct function parameters' order
    Add try/catch block so browse category construction can go on without erring entry.
    
    https://marmot-support.atlassian.net/browse/MHD-8966

--- vufind/web/sys/LocalEnrichment/FavoriteHandler.php
+++ vufind/web/sys/LocalEnrichment/FavoriteHandler.php
@@ -188,9 +188,14 @@ class FavoriteHandler {
 							$groupedWork = new GroupedWorkDriver($catalogResult);
 							if ($groupedWork->isValid){
 								// Need to re-sort in order to match list order
-								$key = array_search($this->favorites, $catalogResult['id']);
-								if ($key !== false){
-									$catalogResourceList[$key] = $interface->fetch($groupedWork->getBrowseResult());
+								try {
+									$key = array_search($catalogResult['id'], $this->favorites);
+									if ($key !== false){
+										$catalogResourceList[$key] = $interface->fetch($groupedWork->getBrowseResult());
+									}
+								} catch (Exception $e){
+									global $pikaLogger;
+									$pikaLogger->withName(__CLASS__)->error(__METHOD__ . ': ' .$e->getMessage());
 								}
 							}
 						}
