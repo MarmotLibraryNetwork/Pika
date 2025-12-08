e7a30660fa7f36d3c9b7f9e5b3efe17290d27f86
Pascal Brammeier <pascal@marmot.org>
2025-12-08 12:05:13 -0700

    IN-3291 Add logging for OverDrive alternate library searches
    https://marmot.myjetbrains.com/youtrack/issue/IN-3291

--- vufind/web/services/Union/Search.php
+++ vufind/web/services/Union/Search.php
@@ -1,8 +1,7 @@
 <?php
 /*
  * Pika Discovery Layer
- * Copyright (C) 2023  Marmot Library Network
- *
+ * Copyright (C) 2025  Marmot Library Network
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -48,6 +47,8 @@ class Union_Search extends Action {
 			$type    = $_REQUEST['basicType'] ?? $_REQUEST['type'];
 			$lookfor = $_REQUEST['lookfor'] ?? '';
 			$link    = SearchSources::getExternalLink($searchSource, $type, $lookfor);
+			global $pikaLogger;
+			$pikaLogger->withName(__CLASS__)->info("External link for redirect is: $link");
 			header('Location: ' . $link);
 			die();
 		}else{
