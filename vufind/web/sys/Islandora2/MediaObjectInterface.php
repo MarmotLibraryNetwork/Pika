067dcec35fe358c88a6320e2766bbbbff701554a
Chris <chris@marmot.org>
2025-12-08 13:17:25 -0700

    Rename getPrimaryFile method to getOriginalMedia in MediaObjectInterface

--- vufind/web/sys/Islandora2/MediaObjectInterface.php
+++ vufind/web/sys/Islandora2/MediaObjectInterface.php
@@ -69,7 +69,7 @@ interface MediaObjectInterface
      *
      * @return array|null
      */
-    public function getPrimaryFile(): ?array;
+    public function getOriginalMedia(): ?array;
 
     /**
      * Optional hook for classes to report whether they are able to represent
