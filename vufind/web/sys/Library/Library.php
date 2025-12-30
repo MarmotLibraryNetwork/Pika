69dce664a0706d95336d3ec99dfdb2445da7f478
Pascal Brammeier <pascal@marmot.org>
2025-12-30 11:12:47 -0700

    D-4285 When setting Archive Only library, turn off restricted search
    https://marmot.myjetbrains.com/youtrack/issue/D-4285
    
    Turn off restrict search by library. Also set the repeat search option to none.

--- vufind/web/sys/Library/Library.php
+++ vufind/web/sys/Library/Library.php
@@ -1,8 +1,7 @@
 <?php
 /*
  * Pika Discovery Layer
- * Copyright (C) 2023  Marmot Library Network
- *
+ * Copyright (C) 2025  Marmot Library Network
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -1599,6 +1598,8 @@ class Library extends DB_DataObject {
 		$this->showFavorites             = false;
 		$this->showRatings               = false;
 		$this->hideCommentsWithBadWords  = false;
+		$this->restrictSearchByLibrary   = false;
+		$this->repeatSearchOption        = 'none';
 	}
 
 	/**
