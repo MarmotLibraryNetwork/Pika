eb4cd4916035e49a8270a9be79e4795f2135925e
Pascal Brammeier <pascal@marmot.org>
2026-01-09 15:35:18 -0700

    Log when item callnumber subfield has more than one entry in an item.

--- vufind/reindexer/src/org/pika/IlsRecordProcessor.java
+++ vufind/reindexer/src/org/pika/IlsRecordProcessor.java
@@ -1062,9 +1062,9 @@ abstract class IlsRecordProcessor extends MarcRecordProcessor {
 			volume = getItemSubfieldData(volumeSubfield, itemField);
 
 			if (useItemBasedCallNumbers) {
-				String callNumberPreStamp  = getItemSubfieldDataWithoutTrimming(callNumberPrestampSubfield, itemField);
-				String callNumber          = getItemSubfieldDataWithoutTrimming(callNumberSubfield, itemField);
-				String callNumberCutter    = getItemSubfieldDataWithoutTrimming(callNumberCutterSubfield, itemField);
+				String callNumberPreStamp  = getItemSubfieldDataWithoutTrimming(callNumberPrestampSubfield, itemField, itemInfo);
+				String callNumber          = getItemSubfieldDataWithoutTrimming(callNumberSubfield, itemField, itemInfo);
+				String callNumberCutter    = getItemSubfieldDataWithoutTrimming(callNumberCutterSubfield, itemField, itemInfo);
 				String callNumberPostStamp = getItemSubfieldData(callNumberPoststampSubfield, itemField);
 
 				StringBuilder fullCallNumber     = new StringBuilder();
@@ -1276,6 +1276,10 @@ abstract class IlsRecordProcessor extends MarcRecordProcessor {
 	}
 
 	private String getItemSubfieldDataWithoutTrimming(char subfieldIndicator, DataField itemField) {
+		return getItemSubfieldDataWithoutTrimming(subfieldIndicator, itemField, null);
+	}
+
+	private String getItemSubfieldDataWithoutTrimming(char subfieldIndicator, DataField itemField, ItemInfo iteminfo) {
 		if (subfieldIndicator == ' '){
 			return null;
 		}else {
@@ -1287,6 +1291,10 @@ abstract class IlsRecordProcessor extends MarcRecordProcessor {
 			} else if (subfields.isEmpty()) {
 				return null;
 			} else {
+				if (fullReindex && iteminfo != null) {
+					// Report these to the cataloging team
+					logger.warn("Item callnumber subfield {} has more than one entry on item {}", subfieldIndicator, iteminfo.getItemIdentifier());
+				}
 				StringBuilder subfieldData = new StringBuilder();
 				for (Subfield subfield:subfields) {
 					appendWithSpace(subfieldData, subfield.getData());
