292d3e97f8eb9be01be6473784d9e1995703eb31
Chris <chris@marmot.org>
2026-03-25 08:49:08 -0600

    #D-5010 Add Organization controller for corporate body taxonomy display
    Implements a new controller that handles displaying corporate body taxonomy term details. The controller validates the taxonomy type, extracts all relevant organization data, and passes it to the template for rendering.
    
    This enables users to view comprehensive organizational information including basic details, notes, related places, and related organizations through a dedicated display interface.

---
+++ vufind/web/services/Archive2/Organization.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * Pika Discovery Layer
+ * Copyright (C) 2026  Marmot Library Network
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Archive2;
+
+require_once ROOT_DIR . '/services/Archive2/TaxonomyObject.php';
+require_once ROOT_DIR . '/sys/Islandora2/CorporateBodyTaxonomy.php';
+
+use Islandora2\CorporateBodyTaxonomy;
+
+/**
+ * Display controller for Islandora 2 Corporate Body taxonomy terms.
+ *
+ * URL: /Archive2/Organization?tid={tid}
+ */
+class Organization extends TaxonomyObject
+{
+    public function launch()
+    {
+        global $interface;
+
+        if (!($this->taxonomyObject instanceof CorporateBodyTaxonomy)) {
+            $this->logger->error('Organization controller received wrong taxonomy type.', [
+                'tid'      => $_GET['tid'] ?? null,
+                'received' => $this->taxonomyObject ? get_class($this->taxonomyObject) : 'null',
+            ]);
+            return;
+        }
+
+        $org = $this->taxonomyObject;
+
+        $interface->assign('alternate_name',    $org->getAlternateName());
+        $interface->assign('founded_year',      $org->getFoundedYear());
+        $interface->assign('dissolved_year',    $org->getDissolvedYear());
+        $interface->assign('notes',             $org->getNotes());
+        $interface->assign('organization_type', $org->getOrganizationType());
+        $interface->assign('organization_url',  $org->getOrganizationUrl());
+        $interface->assign('related_place',     $org->getRelatedPlace());
+        $interface->assign('related_organization', $org->getRelatedOrganization());
+
+        parent::launch();
+
+        $interface->assign('taxonomy_type_template', 'taxonomy_corporate_body');
+
+        $title = $this->taxonomyObject->getTitle();
+        parent::display('taxonomy_wrapper.tpl', $title);
+    }
+}
