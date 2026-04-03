69c54f725c483de9d903f837edec96bceae35f48
C.J. Ohara <cj@marmot.org>
2026-04-03 09:56:16 -0600

    D-5305 Security Audit - Directory Traversal   added logging and comment.

--- vufind/web/genealogyImage.php
+++ vufind/web/genealogyImage.php
@@ -43,8 +43,15 @@ switch ($_GET['size']){
 
 global $configArray;
 $fileName = $configArray['Genealogy']['imagePath'] .DIR_SEP. $sizeFolder . DIR_SEP . $_GET['image'];
+// We need to check to see if someone is trying to switch directories if they are we log the error and return false
 if (!str_contains($_GET['image'], '/') && file_exists($fileName)){
 	[, , $imageType] = getimagesize($fileName);
 	header("Content-type: " . image_type_to_mime_type($imageType));
 	readfile($fileName);
-}else {return false;}
+}else {
+	if (str_contains($_GET['image'], '/')){
+		global $pikaLogger;
+		$pikaLogger->warn("Image contained invalid character /", $_GET['image']);
+	}
+	return false;
+}
