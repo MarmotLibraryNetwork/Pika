88ca489bc2f5752f9c854c25b4e58cd2cddfae7a
C.J. Ohara <cj@marmot.org>
2026-06-04 12:17:28 -0600

    D-5018 Implement Islandoora2 User Lists add to list from search results.

--- vufind/web/sys/DBMaintenance/islandora_updates.php
+++ vufind/web/sys/DBMaintenance/islandora_updates.php
@@ -1,5 +1,4 @@
 <?php
-@ini_set('memory_limit',-1);
 /*
  * Pika Discovery Layer
  * Copyright (C) 2026  Marmot Library Network
@@ -484,22 +483,25 @@ function getTidFromNidAuthorship(){
 function convertListPidToNid():bool {
 	global $pikaLogger;
 	require_once ROOT_DIR . '/sys/LocalEnrichment/UserListEntry.php';
+	require_once ROOT_DIR . '/sys/Library/Library.php';
+	require_once ROOT_DIR . '/sys/SearchObject/Factory.php';
+
+	$library = new Library();
+	$library->whereAdd('archiveNamespace IS NOT NULL && archiveNamespace != ""');
+	$library->find();
+	$nameSpace = $library->fetchAll('archiveNamespace');
+
+	/** @var SearchObject_Islandora2 $islandora2Search */
+	$islandora2Search = SearchObjectFactory::initSearchObject('Islandora2');
+
 	$userListEntry = new UserListEntry();
 	$userListEntry->find();
 	$success = false;
-	while($userListEntry->fetch()){
+	while ($userListEntry->fetch()){
 		$pid   = $userListEntry->groupedWorkPermanentId;
 		$parts = explode(':', $pid);
 		if (count($parts) > 1){
-			require_once ROOT_DIR . '/sys/Library/Library.php';
-			$library = new Library();
-			$library->whereAdd('archiveNamespace IS NOT NULL && archiveNamespace != ""');
-			$library->find();
-			$nameSpace = $library->fetchAll('archiveNamespace');
 			if (in_array($parts[0], $nameSpace)){
-				require_once ROOT_DIR . '/sys/SearchObject/Factory.php';
-				/** @var SearchObject_Islandora2 $islandora2Search */
-				$islandora2Search = SearchObjectFactory::initSearchObject('Islandora2');
 				if ($nids = $islandora2Search->getNodeIdsbyLegacyPIDs([$pid])){
 					foreach ($nids as $nid){
 						$userListEntry->groupedWorkPermanentId = $nid;
@@ -512,7 +514,6 @@ function convertListPidToNid():bool {
 					$success = false;
 				}
 			}else{
-
 				$pikaLogger->warn("The object may be a taxonomy", $parts);
 				$userListEntry->hidden = true;
 				$userListEntry->update();
