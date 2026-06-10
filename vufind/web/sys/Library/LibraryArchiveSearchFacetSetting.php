0efed927725f4f9f0291f60cb503293e02b2da10
Pascal Brammeier <pascal@marmot.org>
2026-06-10 10:53:40 -0600

    D-5015 Clean out old facet labels
    https://marmot.myjetbrains.com/youtrack/issue/D-5015

--- vufind/web/sys/Library/LibraryArchiveSearchFacetSetting.php
+++ vufind/web/sys/Library/LibraryArchiveSearchFacetSetting.php
@@ -19,7 +19,8 @@
 require_once ROOT_DIR . '/sys/Search/FacetSetting.php';
 
 class LibraryArchiveSearchFacetSetting extends FacetSetting {
-	public $__table = 'library_archive_search_facet_setting';    // table name
+	const string ISLANDORA_FACET_INI = 'islandoraFacets';
+	public $__table                  = 'library_archive_search_facet_setting';    // table name
 	public $libraryId;
 
 	static $defaultFacetList = [
@@ -39,7 +40,7 @@ class LibraryArchiveSearchFacetSetting extends FacetSetting {
 		$library = new Library();
 		$library->orderBy('displayName');
 		if (UserAccount::userHasRoleFromList(['libraryAdmin', 'libraryManager'])){
-			$homeLibrary = UserAccount::getUserHomeLibrary();
+			$homeLibrary        = UserAccount::getUserHomeLibrary();
 			$library->libraryId = $homeLibrary->libraryId;
 		}
 		$library->find();
@@ -59,7 +60,7 @@ class LibraryArchiveSearchFacetSetting extends FacetSetting {
 	}
 
 	static public function getAvailableFacets(){
-		$config            = getExtraConfigArray('islandoraFacets');
+		$config          = getExtraConfigArray(self::ISLANDORA_FACET_INI);
 		$availableFacets = $config['Results'] ?? self::$defaultFacetList;
 		return $availableFacets;
 	}
