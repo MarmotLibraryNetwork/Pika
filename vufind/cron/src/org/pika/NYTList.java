8f8c5868c930a2aac1216e33bb68d940ad16c027
Pascal Brammeier <pascal@marmot.org>
2026-09-03 15:54:51 -0600

    D-5314 Wait 13 secs between NYTimes requests
    With the 12 second delay, the cron process is still exceeding the quota limit. Trying an additional second to see if that improves our success.
    
    https://marmot.myjetbrains.com/youtrack/issue/D-5314

--- vufind/cron/src/org/pika/NYTList.java
+++ vufind/cron/src/org/pika/NYTList.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class NYTList implements IProcessHandler {
 	// The NY Times API allows 5 requests per minute, so wait 12 seconds between calls that reach it.
-	private static final long NYT_API_CALL_DELAY = 12000L;
+	private static final long NYT_API_CALL_DELAY = 13000L;
 
 	private PikaSystemVariables systemVariables;
 	private String userAgent;
@@ -121,6 +121,7 @@ public class NYTList implements IProcessHandler {
 				processEntry.incErrors();
 				return;
 			}
+			logger.info("Got NY Times list of lists: {}", result);
 			JSONArray results = result.getJSONObject("results").getJSONArray("lists");
 			for (int i = 0; i < results.length(); i++) {
 				JSONObject    newResult         = (JSONObject) results.get(i);
